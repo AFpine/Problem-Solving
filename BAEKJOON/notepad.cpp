788a92e07379b5118fe5b179d5ae04753cb52859
AFpine <afpine@naver.com>
2022-03-08 14:20:27 +0900

    ps update 220308

--- BAEKJOON/notepad.cpp
+++ BAEKJOON/notepad.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <set>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
-#include <numeric>
-#include <limits>
-#include <cstdint>
+#include <bits/stdc++.h>
 
 // 12865 Standard Backpack (Knapsack Problem)
 
