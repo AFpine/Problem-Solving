a288c38a86ee89d4c8ac207add568462bbb8fa45
AFpine <afpine527@gmail.com>
2022-03-19 18:16:18 +0900

    ps update

--- BOJ/notepad.cpp
+++ BOJ/notepad.cpp
@@ -1,36 +1,11 @@
 #include <bits/stdc++.h>
 
-// 1167 Diameter of tree (DFS, DIameter of tree proof)
+// 
 
-// update 220313
+// update 220319
 
 using namespace std;
 
-int diameter = 0;
-bool visited[100001];
-int far = 1;
-
-struct Node
-{
-	vector<pair<int,int>> edge;
-};
-
-void DFS(vector<Node>& v,int now, int nowdia)
-{
-	if(diameter < nowdia)
-	{
-		diameter = nowdia;
-		far = now;
-	}
-	for(auto x : v[now].edge)
-	{
-		if(visited[x.first] == false)
-		{
-			visited[x.first] = true;
-			DFS(v,x.first,nowdia+x.second);
-		}
-	}
-}
 
 int main()
 {
@@ -38,40 +13,8 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	int a,b,c;
-	cin>>n;
-	vector<Node> tree(100001);
+	cout<<"Hello world!\n";
 
-	for(int i = 1;i<=n;++i)
-	{
-		cin>>a;
-		while(1)
-		{
-			cin>>b;
-			if(b == -1)
-			{
-				break;
-			}
-			cin>>c;
-			tree[a].edge.push_back({b,c});
-		}
-	}
+	return 0;
 
-	visited[1] = true;
-	DFS(tree,1,0);
-	for(int j = 1;j<=n;++j)
-	{
-		visited[j] = false;
-	}
-	visited[far] = true;
-	DFS(tree,far,0);
-	for(int j = 1;j<=n;++j)
-	{
-		visited[j] = false;
-	}
-	visited[far] = true;
-	DFS(tree,far,0);
-
-	cout<<diameter<<"\n";
 }
