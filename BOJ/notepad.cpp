96ba12d14b23b2525be1c23a51f19cb75d337302
AFpine <mrt2000@jbnu.ac.kr>
2022-09-28 19:21:42 +0900

    ps update

--- BOJ/notepad.cpp
+++ BOJ/notepad.cpp
@@ -1,36 +1,66 @@
 #include <bits/stdc++.h>
 
-// 9527 Counting 1
+// 23577 Trio
 
-// update 220405
+// update 220926
 
 using namespace std;
+#define sz(a) (int)(a.size()) 
+#define all(a) (a).begin(), (a).end()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<long double, long double> pdd;
+typedef complex<double> cplx;
 
-int main()
-{
+const double PI = acos(-1);
+const int INF = 1e9+5;
+const long long llINF = 1e18;
+const int minINF = -2e9-1;
+const long long MOD = 1e9+7;
+const int MAX = 1000005;
+
+int N;
+int cnt[4][10];
+vector<string> arr;
+
+ll f(int idx, const string& s1, const string& s2) {
+	if(idx >= 4) return 0;
+	if(s1[idx] == s2[idx]) {
+		
+	}
+	else {
+		
+	}
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-    long long A,B;
-    long long result = 0;
+	cin>>N;
+	arr.resize(N);
+	for(auto &i : arr) cin>>i;
+	
+	sort(all(arr));
+	for(auto &i : arr) {
+		for(int j = 0; j < 4; ++j) cnt[j][i[j]]++;
+	}
 
-    cin>>A>>B;
+	ll res = 0;
+	for(int i = 0; i < N; ++i) {
+		for(int j = i + 1; j < N; ++j) {
 
-    for(long long i = A; i <= B; ++i)
-    {
-        long long temp = i;
-        while(temp > 0)
-        {
-            if(temp % 2 == 1)
-            {
-                result++;
-            }
-            temp /= 2;
-        }
-    }
-
-    cout<<result;
+		}
+	}
+	
 
 	return 0;
 }
