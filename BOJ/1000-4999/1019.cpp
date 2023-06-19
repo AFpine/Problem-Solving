100e0ad6e76b8fe92b223e21dd2b5ae7815b8daa
AFpine <mrt2000@jbnu.ac.kr>
2023-06-19 10:51:19 +0900

    update

---
+++ BOJ/1000-4999/1019.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sz(a) (int)(a.size()) 
+#define all(a) (a).begin(), (a).end()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<long double, long double> pdd;
+typedef complex<double> cplx;
+
+const double PI = acos(-1);
+const int INF = 1e9+5;
+const long long llINF = 1e18;
+const int minINF = -2e9-1;
+const long long MOD = 1e9+9;
+const int MAX = 105;
+
+int N;
+ll result[13];
+
+void f(int n, int d) {
+	while(n) {
+		result[n%10] += d;
+		n /= 10;
+	}
+}
+
+void solve(int a, int b, int d) {
+	while(a % 10 != 0 && a <= b) {
+		f(a, d);
+		a++;
+	}
+	if(a > b) return;
+	while(b % 10 != 9 && a <= b) {
+		f(b, d);
+		b--;
+	}
+
+	int t = (b/10 - a/10 + 1);
+	for(int i = 0; i < 10; ++i) result[i] += d * t;
+
+	solve(a/10, b/10, d*10);
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin>>N;
+	solve(1, N, 1);
+
+	for(int i = 0; i < 10; ++i) cout<<result[i]<<" ";
+
+	return 0;
+}
